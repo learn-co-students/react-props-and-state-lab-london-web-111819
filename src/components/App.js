035d1693abc8dff95cc86b2c85c8d0d5a1b4dbd8
badogan <basri.dogan.71@gmail.com>
2020-01-29 16:10:28 +0000

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,61 @@ import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
 class App extends React.Component {
-  constructor() {
-    super()
+  
+  BASE_URL = '/api/pets'
 
-    this.state = {
-      pets: [],
-      filters: {
-        type: 'all'
-      }
+  // constructor() {
+  //   super()
+
+  //   this.state = {
+  //     pets: [],
+  //     filters: {
+  //       type: 'all'
+  //     }
+  //   }
+  // }
+
+  state = {
+    pets: [],
+    filters: {
+      type: 'all'
     }
+  }
+
+  updateStatePetsArray = (arrayInput) => {
+    this.setState({
+      ...this.state,
+      pets: arrayInput
+    })
+  }
+
+  handleFindPets = () => {
+    //TODO: Fetch a list of pets using fetch
+    {
+      this.state.filters.type === 'all' 
+      ? fetch(this.BASE_URL).then(object=>object.json()).then(this.updateStatePetsArray)
+      : fetch(`${this.BASE_URL}?type=${this.state.filters.type}`).then(object=>object.json()).then(this.updateStatePetsArray)
+    }
+  }
+
+  handleAdoptPet = (receivedId) => {
+    console.log(receivedId) //Works!
+    //Find the pet with the id from the array of pets (which is in state)
+    const targetPetObject = this.state.pets.find(item=>{return(item.id===receivedId)}) //Works
+    const indexOfTargetPet = this.state.pets.indexOf(targetPetObject) //Works
+    console.log(indexOfTargetPet)
+    this.setState({
+      ...this.state,
+      pets: this.state.pets.map((item,index)=>index!==indexOfTargetPet ?item :{...item,isAdopted:true})
+    })
+  }
+
+  handleChangeType = (newType) => {
+    this.setState({
+      filters: {
+        type: newType
+      }
+    })
   }
 
   render() {
@@ -24,10 +70,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onFindPets={this.handleFindPets} onChangeType={this.handleChangeType} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.handleAdoptPet}/>
             </div>
           </div>
         </div>
