e2938ffba3a7d9b116fcc5db71cd01708e8d3777
github username <github email address>
2020-01-31 15:01:58 +0000

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,42 @@ import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
 class App extends React.Component {
-  constructor() {
-    super()
-
-    this.state = {
+ 
+  state = {
       pets: [],
       filters: {
         type: 'all'
       }
-    }
   }
+
+  adoptPet = (petId) => {
+
+    //long version of not destructively changing original array of json
+    const pets = [...this.state.pets]
+    const petToChange = pets.find(pet => pet.id === petId)
+    const CopyOfpetToChange = {...petToChange}
+    CopyOfpetToChange.isAdopted = true 
+    const indexToSwap = pets.indexOf(petToChange)
+    pets[indexToSwap] = CopyOfpetToChange
+    
+    this.setState({ pets: pets})
+  }
+
+  getPets = () => {
+    const url = this.state.filters.type === 'all' 
+    ? '/api/pets'
+    : `/api/pets?type=${this.state.filters.type}`
+
+    fetch(url)
+    .then(resp => resp.json())
+    .then(pets => this.setState({pets: pets}))
+    
+  }
+
+  updateFilter = (e) => { //use spread operator when setting state, incase theres more stuff in there that we dont want to use/lose/change
+    this.setState({ filters: {...this.state.filters,
+      type: e.target.value}})
+  }  
 
   render() {
     return (
@@ -24,10 +50,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters updateFilter={this.updateFilter} getPets={this.getPets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} adoptPet={this.adoptPet}/>
             </div>
           </div>
         </div>
